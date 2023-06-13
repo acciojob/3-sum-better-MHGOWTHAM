1ad957c781ecad59e4959142cb25d52de0780f41
GOWTHAM M H <117714418+MHGOWTHAM@users.noreply.github.com>
2023-06-13 17:40:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 function threeSum(arr, target) {
 // write your code here
-  
+	let sum=0
+  for(let i=0;i<arr.length;i++){
+	  sum+=arr[i]
+	  if(sum>=target){
+		  return sum;
+	  }
+  }
 }
 
 module.exports = threeSum;
